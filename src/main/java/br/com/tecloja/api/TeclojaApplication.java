a4c9c3ef6a5afc7522d2ab37ea79358e25c32ad5
PedroHBranco-bit <pedro.dominato0515@gmail.com>
2026-06-01 22:40:37 -0300

    Update TeclojaApplication.java

--- src/main/java/br/com/tecloja/api/TeclojaApplication.java
+++ src/main/java/br/com/tecloja/api/TeclojaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TeclojaApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TeclojaApplication.class, args);
 	}
